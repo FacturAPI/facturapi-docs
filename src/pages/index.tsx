e92f1eec62005e4a2598585bd4970d0ed15b011d
javorosas <javier.rosasb@gmail.com>
2021-10-07 22:28:13 +0200

    change CTA on home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,13 +11,13 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <h1 className="hero__title">Facturapi</h1>
+        <p className="hero__subtitle">Documentación oficial</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Introducción - 2min ⏱️
           </Link>
         </div>
       </div>

2a0838532d1a0f88afa4ad5151eb1a094fc8915f
edgtz <95707654+edgtz@users.noreply.github.com>
2024-07-09 11:29:38 -0700

    Update docusaurus.config.js (#137)

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -80,8 +80,7 @@ const darkCodeTheme = require("prism-react-renderer").themes.dracula;
               type: 'dropdown',
               label: 'Referencia API',
               items: [
-                { href: '/api', label: 'v2 (CFDI v4.0) ✅' },
-                { href: '/api_v1', label: 'v1 (CFDI v3.3) 🚧' },
+                { href: '/api', label: 'v2 (CFDI v4.0) ✅' }
               ]
             },
             {

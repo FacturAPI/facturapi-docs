eb224cde0c0e223700d97ac7374266240fb38362
Javier Rosas <javier.rosasb@gmail.com>
2026-05-05 21:30:18 +0200

    [codex] Add rate limit documentation (#252)
    * Add rate limit documentation
    
    * Fix React return types
    
    * chore(warningInfo): add disclaimer in rate limits
    
    ---------
    
    Co-authored-by: raul@facturapi.io <raul@facturapi.io>
    Co-authored-by: Raúl García <141654545+raul-facturapi@users.noreply.github.com>

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -28,7 +28,7 @@ function HomepageHeader() {
   );
 }
 
-export default function Home(): JSX.Element {
+export default function Home(): React.ReactElement {
   return (
     <Layout>
       <HomepageHeader />
